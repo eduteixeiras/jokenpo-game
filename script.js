ef83a81518d5f28e0bd755f89e8367471a5d6288
eduteixeiras <ceteixeira04@gmail.com>
2025-07-25 12:26:28 -0300

    COMENTÁRIOS

--- script.js
+++ script.js
@@ -5,16 +5,19 @@ const machineScore = document.querySelector("#machine-score");
 let humanScoreFinal = 0;
 let machineScoreFinal = 0;
 
+//OPÇÕES QUE SERÃO USADAS NO JOGO
 const OPCOES = {
     ROCK: 'rock',
     PAPER: 'paper',
     SCISSOR: 'scissor'
 }
 
+//VEZ DA PESSOA
 const playHuman = (humanChoice) => {
     playTheGame(humanChoice, playMachine())
 }
 
+//VEZ DA MÁQUINA
 const playMachine = () => {
     const choices = [OPCOES.ROCK, OPCOES.PAPER, OPCOES.SCISSOR];
     const randomNumber = Math.floor(Math.random()*3);
@@ -22,10 +25,11 @@ const playMachine = () => {
     return choices[randomNumber]
 }
 
+//REALIZA A VERIFICÃO DO RESULTADO
 const playTheGame = (human, machine) => {
-    if (human === machine) {
-        result.innerHTML = "Deu empate"
-    } else if(
+    if (human === machine) { //COMPARA O VALOR DA PESSOA E DA MÁQUINA E VÊ SE SÃO IGUAIS
+        result.innerHTML = "Deu empate!"
+    } else if( //FAZ A COMPARAÇÃO PARA SABER SE A PESSOA GANHOU
         (human === OPCOES.ROCK && machine === OPCOES.SCISSOR) ||
         (human === OPCOES.PAPER && machine === OPCOES.ROCK) ||
         (human === OPCOES.SCISSOR && machine === OPCOES.PAPER))
